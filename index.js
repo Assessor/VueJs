2dfbffa63e782c3991d765ba8fac84cb2a1f86bb
Ivan Maltsev <ivan.maltsev78@gmail.com>
2018-04-15 10:18:11 +0500

    Add logs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const car = (name, model, owner, year, phone, image) =>  ({ name, model, owner, year, phone, image });
+const logs = (text, type, date = new Date()) => ({text, type, date});
 
 const cars = [
     car('Ford', 'Focus', 'Max', 2016, '+7 929 123 45 67', 'ImagesIndex/focus.jpg'),
@@ -9,6 +10,41 @@ const cars = [
 new Vue({
     el: '#app',
     data: {
-        cars: cars
+        cars: cars,
+        car: cars[0],
+        selectedCarIndex: 0,
+        phoneVisibility: false,
+        search: '',
+        modalVisibility: false,
+        logs: []
+    },
+    methods: {
+        selectCar: function(index) {
+            this.selectedCarIndex = index;
+            this.car = cars[index];
+        },
+        newOrder: function() {
+            this.modalVisibility = false;
+            this.logs.push(logs('Заказ добавлен', 'Ok'));
+        },
+        cancelOrder: function() {
+            this.modalVisibility = false;
+            this.logs.push(logs('Заказ отменен', 'Cnl'));
+        }
+    },
+    computed: {
+        phoneBtnText() {
+            return this.phoneVisibility ? 'Hide phone' : 'Show phone'
+        },
+        filteredCars() {
+            return this.cars.filter(car => {
+                return car.name.indexOf(this.search) > -1 || car.model.indexOf(this.search) > -1
+            });
+        }
+    },
+    filters: {
+        date(value) {
+            return value.toLocaleString();
+        }
     }
 });
